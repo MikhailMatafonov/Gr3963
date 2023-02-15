31256f95aef52f4ecb11cd899961b38640b47ff5
Михаил Матафонов <Matmix2015@yandex.ru>
2023-02-15 21:07:07 +0300

    add solution for task 4 Home

---
+++ Sem1Task4Home/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу , которая принимает на вход три числа и выдает максимальное из этих чисел
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int C = Convert.ToInt32(Console.ReadLine());
+if(A > B)
+{
+    if(A > C)
+    {
+        Console.WriteLine($"Число {A} является максимальным");
+    }
+    else
+    {
+        Console.WriteLine($"Число {C} является максимальным");
+    }
+}    
+else
+{
+if(B > C)
+    Console.WriteLine($"Число {B} является максимальным");
+    else
+    {
+    Console.WriteLine($"Число {C} является максимальным");
+    }
+}
+
+
+        
+    
+    
