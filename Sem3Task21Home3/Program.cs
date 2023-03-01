f9275e493fae2fe0cd3655f49abfcf7c3fb877ba
Михаил Матафонов <Matmix2015@yandex.ru>
2023-03-01 19:41:19 +0300

    add solution for task 21,23

---
+++ Sem3Task21Home3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadData(string num)
+{
+    Console.WriteLine(num);
+    return int.Parse(Console.ReadLine()??"0");
+}
+//Метод выдает данные пользователя
+void PrintData(string num, double val)
+{
+    Console.WriteLine(num + val);
+}
+//Метод находит расстояние между точками на плоскости
+double CalcLen3D(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+//  return Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y1 - y2),2)) + Math.Pow((z2 - z1),2))); альтернативный пример
+}
+
+int x1 = ReadData("Введите координату x точки A:");
+int y1 = ReadData("Введите координату y точки A:");
+int z1 = ReadData("Введите координату z точки A:");
+int x2 = ReadData("Введите координату x точки B:");
+int y2 = ReadData("Введите координату y точки B:");
+int z2 = ReadData("Введите координату z точки B:");
+
+double res = CalcLen3D(x1, x2, y1, y2, z1, z2); //выводим результат, передаем полученные данные
+
+PrintData("Растояние между точками A и B = ", res);
+
+//Сделать метод загрузки точек?*
---
+++ Sem3Task23Home3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу кубов чисел от 1 до N.1
+
+
+//Метод читает данные от пользователя
+//<тип> <имя метода> (<тип> <название переменной>)
+// {
+//     return <тип> (return - возвращаем)
+// }
+int ReadInput(string num)
+{
+    Console.WriteLine(num);
+    return int.Parse(Console.ReadLine()??"0");
+}
+string LineBuilder(int num, int pow)
+{
+    string result = String.Empty;
+//    pow = 3;
+    for (int i = 1; i <= num; i++) // "вводим ограничения"
+        {
+            result = (result + Math.Pow(i,pow+1)+"\t"); //вычисляем куб заданного числа
+        }
+    return result;
+}
+int N = ReadInput("Введите число: ");
+Console.WriteLine(LineBuilder(N,1 - 1)); //выводим первую строку в консоли
+Console.WriteLine(LineBuilder(N,2)); //выводим вторую строку в консоли
